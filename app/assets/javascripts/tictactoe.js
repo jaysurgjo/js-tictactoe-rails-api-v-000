8d428bbeae2ce0c476b637b41f3ea485c9fee941
Jordan Surgick <jordansurgick@aol.com>
2020-05-31 18:44:07 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,160 +1,208 @@
-var turn = 0;
-var currentGame = 0;
+$(attachListeners)
 
-$(document).ready(function(){
-	attachListeners();
-});
-
-
-function player() {
-	if(turn % 2 == 0 ) {
-		return "X";
-	} else {
-		return "O";
-	}
-}
-
-function updateState(square) {
-	let currentPlayer = player();
-	$(square).append(currentPlayer);
-}
-
-function setMessage(string) {
-	$("div#message").append(string);
-}
-
-
-function checkWinner() {
-	var board = {};
-	var winner = false;
-	// returns true when a player wins horizontally
-	// returns true when a player wins diagonally
-	// returns true when a player wins vertically
-	// returns false if no winning combination is present on the board
-
-	$('td').text( (index, square) => board[index] = square);
-	let winCombinations = [
-	  [0, 1, 2],
-	  [3, 4, 5],
-	  [6, 7, 8],
-	  [0, 3, 6],
-	  [1, 4, 7],
-	  [2, 5, 8],
-	  [0, 4, 8],
-	  [6, 4, 2]
-	];
-
-	winCombinations.forEach(function(position) {
-		if (board[position[0]] === board[position[1]] && board[position[1]] === board[position[2]] && board[position[0]] !== "") {
-		setMessage(`Player ${board[position[0]]} Won!`)
-		winner = true;
-		saveGame();
-		}
-	})
-	return winner;
-}
-
-function doTurn(square) {
-	updateState(square);
-	turn++;
-
-	if (checkWinner(turn) === true) {
-		saveGame();
-		clearGame();
-	} else if (turn === 9) {
-		saveGame();
-		turn = 0;
-		setMessage(`Tie game.`);
-		clearGame();
-	};
-}
-
-function gameOver() {
-	let gameOver = false;
-	if (checkWinner() === true || setMessage() === `Tie game.`) {
-	gameOver = true;
-
-};
-	return gameOver;
-}
-
-
-function attachListeners(){
-	$('td').on("click", function() {
-	// check if square is empty and if so allow the player to take turn
-	if (this.innerHTML === "" && gameOver() === false) {
-		doTurn(this);
-	};
-
-	});
-	$('#previous').on("click", function() {
-			previousGame();
-		});
-	$('#save').on("click", function() {
-		saveGame();
-	});
-	$('#clear').on("click", function() {
-		clearGame();
-	});
-}
-
-function saveGame() {
-	let state = [];
-
-
-	$('td').text((index, square) => {
-    state[index] = square;
+function attachListeners() {
+  //on click add event listener to td element
+  $("tbody").click(function(event) {
+    doTurn(event);
   });
-	var gameData = { state: state }
 
-  if (currentGame) {
-    $.ajax({
-      type: 'PATCH',
-      url: `/games/${currentGame}`,
-      data: gameData
-    });
+  // show previous games
+  $("#previous").click(function(event) {
+    previousGames() 
+  })
+
+  //save current game
+  $("#save").on("click", function(event){
+    save(false)
+
+  })
+
+  $(document).on("click", "li", function(event){    
+    var state = $(this).data("state").split(",")
+    var id = $(this).data("id")
+    loadBoard(state, id);
+    turn = getTurn()
+    //history.pushState(null,null,`/games/${id}`)
+  })
+}
+
+var turn = 0
+var currentGame = getBoard()
+var currentGameId = undefined
+var winningCombos = [
+ [[0,0],[0,1],[0,2]], 
+ [[1,0],[1,1],[1,2]], 
+ [[2,0],[2,1],[2,2]], 
+ [[0,0],[1,0],[2,0]], 
+ [[0,1],[1,1],[2,1]], 
+ [[0,2],[1,2],[2,2]], 
+ [[0,0],[1,1],[2,2]], 
+ [[2,0],[1,1],[0,2]]]
+
+function doTurn(event) {
+  updateState(event);
+  if (checkWinner()) {
+    save(true);
+    resetBoard();
+    resetTurn();
   } else {
-    $.post('/games', gameData, function(game) {
-      currentGame = game.data.id
-      $("#games").append(`<button id="game-${game.data.id}" onClick="loadGame(${game.data.id})">${game.data.id}</button><br>`);
-    });
+    turn++
   }
 }
 
-
-
-function previousGame() {
-    $.get('/games', function(response) {
-    	const games = response.data;
-    	var list = $('div#games').text();
-
-    	games.forEach(function(game) {
-    		if (!list.includes(game.id)) {
-    			// iterate over each game button add an event listener to each button
-    		$("#games").append(`<button id="game-${game.id}" onClick="loadGame(${game.id})">${game['id']}</button><br>`);
-    		};
-    	});
-    });
+function updateState(event) {
+  //event["currentTarget"].val = player()
+  $(event.target).html(player());
 }
 
-function loadGame(id) {
-	$.get('/games/' + id, function(response) {
-	let state = response.data.attributes.state;
-    let $tds = $('td');
-    for (let index = 0; index < state.length; index++) {
-      $tds[index].innerHTML = state[index];
-      if (state[index].innerHTML !== "") {turn++;};
-    };
-    currentGame = id;
-    turn = state.join('').length;
+function checkWinner() {
+  var gameOver = false
+  // Should evaluate the board to see if anyone has won
+  winningCombos.forEach(function(combo) {
+    var cell1 = cellValue(combo[0][0], combo[0][1]);
+    var cell2 = cellValue(combo[1][0], combo[1][1]);
+    var cell3 = cellValue(combo[2][0], combo[2][1]);
 
-	});
+    if (cell1 == player() && cell2 == player() && cell3 == player()) {
+      // should then pass this string to message()
+      // should make one of two strings: "Player X Won!" or "Player O Won!"
+      message("Player " + player() + " Won!");
+      gameOver = true
+    }
+    // Calls on 'message' and passes it the string 'Tie game' when there is a tie
+    if (tie()) {
+      message("Tie game")
+      gameOver = true
+      
+      console.log("after tie(), before save()")
+    }
+  })
+  return gameOver
 }
 
+function player(){ 
+  if ((turn % 2) == 0) {
+    return "X"
+  } else {
+    return "O"
+  }
+ }
 
-function clearGame() {
-	$('td').empty();
-	board = {};
-	turn = 0;
-	currentGame = 0;
+function tie(){
+  //if turn is last and no empty spaces on board
+  if (lastTurn() && boardFull()) {
+    console.log("inside tie()")
+    return true
+  }
 }
+
+function lastTurn(){
+  if (turn == 8) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function getBoard(){
+  board = []
+  $("tbody td").each(function(){
+    board.push(this.innerHTML)  
+  })
+  return board
+}
+
+function boardFull(board){
+  board = getBoard()
+  return board.every(item => item != "")
+}
+
+function resetBoard(){
+  $("tbody td").text("")
+  //debugger
+}
+
+function loadBoard(gameState, id){
+  currentGameId = id
+  var i = 0
+    $("tbody td").each(function(){
+      $(this).text(gameState[i])
+      i++
+    })
+  }
+
+function resetTurn(){
+  turn = 0
+}
+
+function message(string){
+  $("#message").html(string)
+}
+
+function cellValue(data1, data2) {
+  return $('[data-x="' + data1 + '"][data-y="' + data2 + '"]').html();
+}
+
+function getTurn(){
+  board = getBoard()
+  turnCounter = 0
+  for (i=0; i<board.length; i++){
+    if (board[i] === "X" || board[i] === "O") {
+      turnCounter += 1
+    }
+  }
+  return turnCounter
+}
+
+function save(resetCurrentGameId) {
+  console.log("inside save()")
+  console.log(currentGameId)
+  var url, method;
+
+  if (currentGameId) {
+    console.log("inside patch")
+    method = 'PATCH'
+    url = `/games/${currentGameId}`
+
+  } else {
+    console.log("inside post")
+    method = 'POST'
+    url = `/games`
+  }
+  console.log(url)
+  $.ajax({
+      method: method,
+      url: url,
+      dataType: "json",
+      data: { game: { state: getBoard() }}
+    }).success(function(response){
+      //creates game by status but doesnt hit this success message
+      //i get a internal server error post.
+      //alert("post/patch request success");
+      if (resetCurrentGameId) {
+        currentGameId = undefined
+      } else {
+
+        currentGameId = response.game.id
+      }
+      console.log(currentGameId)
+    }).error(function(error){
+      console.log(error)
+    })
+  }
+
+  function previousGames(){
+    $.ajax({
+      method: "GET",
+      url: "/games"
+    }).success(function(data){
+        var savedGames = data["games"]
+        $("#games").html("")
+        if (savedGames.length > 0 ) {
+          $("#games").append("<ul></ul>")
+          for (var key in savedGames) { 
+            $("#games ul").append(`<li data-state= ${savedGames[key]["state"]} data-id = ${savedGames[key]["id"]}> ${savedGames[key]["id"]}</li>`)
+          }
+        } 
+      })
+  }
